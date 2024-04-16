ac0839660277d537cdf1f253292325576a63e101
greetings1012 <ahnqwerty@gmail.com>
2024-04-16 12:43:58 +0900

    feat(Input.tsx): Input 구현

---
+++ storybook/src/components/Display.tsx
@@ -0,0 +1,12 @@
+import Input from "./Input";
+
+export default function Display() {
+  return (
+    <div>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </div>
+  );
+}
---
+++ storybook/src/components/Input.tsx
@@ -0,0 +1,7 @@
+export default function Input() {
+  return (
+    <div>
+      <input placeholder="1234" type="number"></input>
+    </div>
+  );
+}
