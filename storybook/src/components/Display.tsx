435b86a6073741ebecdb710c09c0d228a21a0f62
jayming66 <mjw8941@gmail.com>
2024-04-16 12:47:11 +0900

    feat: input 컴포넌트

--- storybook/src/components/Display.tsx
+++ storybook/src/components/Display.tsx
@@ -1,7 +1,7 @@
+import DisplayInput from './DisplayInput';
+
 export default function Display() {
   return <div>
-    <label>페이먼츠앱의 핵심 기능 : </label>
-    <input id="pay"></input>
+    <DisplayInput></DisplayInput>
   </div>;
 }
-  
