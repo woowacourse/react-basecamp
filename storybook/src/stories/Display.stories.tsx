011069189120ef5f535b9b1a319a521d734aa9cc
Kim Da Eun <midekuna@gmail.com>
2024-04-16 12:59:05 +0900

    test: Storybook에 Input 컴포넌트 렌더링 테스트 추가

---
+++ storybook/src/stories/Display.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Display from '../components/Display';
+
+const meta = {
+  title: 'Display',
+  component: Display,
+} satisfies Meta<typeof Display>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Input: Story = {
+  args: {
+    inputProps: {
+      placeholder: '값을 입력하세요.',
+    },
+  },
+};
