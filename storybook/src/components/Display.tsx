1a85f74d30f466a75898a4a04802dae5a8147ba3
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-04-16 13:07:00 +0900

    [Storybook 미니 실습] 빙봉(김윤경) 미션 제출합니다. (#29)
    * init: storybook 패키지 설치
    
    * feat: Display storybook 생성
    
    * feat: Input storybook 생성

--- storybook/src/components/Display.tsx
+++ storybook/src/components/Display.tsx
@@ -1,7 +1,3 @@
-import DisplayInput from './DisplayInput';
-
 export default function Display() {
-  return <div>
-    <DisplayInput></DisplayInput>
-  </div>;
+  return <div>카드 정보를 등록하고, 확인할 수 있습니다.</div>;
 }
