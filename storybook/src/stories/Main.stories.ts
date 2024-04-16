1899f5d54eaf998889579dc5c2a6ea755c56f351
lurgi <wahoo9040@naver.com>
2024-04-16 12:58:12 +0900

    feat: 스토리북 테스트

---
+++ storybook/src/stories/Main.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import App from "../App";
+
+const meta = {
+  title: "Example/App",
+  component: App,
+} satisfies Meta<typeof App>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const LoggedOut: Story = { name: "테스트" };
