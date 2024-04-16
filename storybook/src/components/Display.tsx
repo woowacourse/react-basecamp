af7a2e3676c5bd51c8f3abd9f0461a35f51ae8c8
jaeml06 <jaeml0630@gmail.com>
2024-04-16 12:47:09 +0900

    test: 간단한 input컴포넌트 구현

---
+++ storybook/src/components/Display.tsx
@@ -0,0 +1,5 @@
+import Input from "./input";
+
+export default function Display() {
+    return <Input></Input>;
+}
---
+++ storybook/src/components/Input.tsx
@@ -0,0 +1,7 @@
+export default function Input() {
+    return (
+        <form>
+            카드 회사 이름을 입력하세요 <input type="text"></input>
+        </form>
+    );
+}
