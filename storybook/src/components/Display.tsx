61f4dede0130cf3ec69e94e59b34ba92d5fff072
Hyelim Choi <HyelimChoi01@gmail.com>
2024-04-16 12:47:16 +0900

    test: storybook 사용

---
+++ storybook/src/components/Display.tsx
@@ -0,0 +1,9 @@
+import Input from "./Input";
+
+export default function Display() {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+}
---
+++ storybook/src/components/Input.tsx
@@ -0,0 +1,7 @@
+export default function Input() {
+  return (
+    <form method="get">
+      카드 회사 이름을 입력해주세요 : <input type="text"></input>
+    </form>
+  );
+}
