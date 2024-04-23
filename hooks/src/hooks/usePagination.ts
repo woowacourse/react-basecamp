1c67b0266dea58fbab4107a21a018ea74612bf5d
Largopie <106071687+Largopie@users.noreply.github.com>
2024-04-23 15:50:10 +0900

    feat: usePagination hooks 구현

---
+++ hooks/src/hooks/usePagination.ts
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+const usePagination = (initialPage = 1) => {
+  const [currentPage, setCurrentPage] = useState(initialPage);
+
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const goToPrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  return { currentPage, goToNextPage, goToPrevPage };
+};
+
+export default usePagination;
