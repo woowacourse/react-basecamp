c1c5e378cea2a388f6fff02375a5d31c83e71353
Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>
2024-04-23 16:03:53 +0900

    [Custom Hook 미니 실습] 썬데이(김유선) 실습 제출합니다. (#65)
    * chore: 스토리북 세팅
    
    Co-authored-by: Yuseon Kim(썬데이) <useon@users.noreply.github.com>
    
    * feat: display 스토리북 생성
    
    * feat: input 스토리북 생성
    
    * feat: input 스토리북 생성
    
    Co-authored-by: Yuseon Kim(썬데이) <useon@users.noreply.github.com>
    
    * feat: react hook 실습 미션 추가
    
    * feat: useImageSearch 커스텀 훅으로 분리
    
    * feat: useFetch 커스텀 훅으로 분리
    
    * feat: usePagination 커스텀 훅 분리
    
    * feat: useImageSearchWithPagination 커스텀 훅 분리
    
    * refactor: ImageGallery 로직 수정
    
    ---------
    
    Co-authored-by: jinsil <jinsil378900@gmail.com>
    Co-authored-by: Yuseon Kim(썬데이) <useon@users.noreply.github.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ hooks/src/hooks/useFetch.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = <T>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error as Error);
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
