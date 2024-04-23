b95dee3decc7311b492beb9d8ebcca5249dcaa4f
jinsil <jinsil378900@gmail.com>
2024-04-23 12:38:34 +0900

    chore: gitignore에 env 추가

---
+++ hooks/src/hooks/useImageSearchWithPagination.ts
@@ -0,0 +1,32 @@
+// useImageSearchWithPagination.ts
+import { useState, useEffect } from "react";
+import useFetch from "./useFetch";
+import usePagination from "./usePagination";
+
+const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY;
+
+interface Image {
+  id: number;
+  webformatURL: string;
+  tags: string;
+}
+
+interface ImageSearchResult {
+  hits: Image[];
+}
+
+const useImageSearchWithPagination = (keyword: string) => {
+  const [images, setImages] = useState<Image[]>([]);
+  const { currentPage, goToNextPage, goToPrevPage } = usePagination();
+  const { data, isLoading, error } = useFetch<ImageSearchResult>(`https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(keyword)}&page=${currentPage}&image_type=photo&pretty=true`);
+
+  useEffect(() => {
+    if (data) {
+      setImages(data.hits);
+    }
+  }, [data]);
+
+  return { images, currentPage, goToNextPage, goToPrevPage, isLoading, error };
+};
+
+export default useImageSearchWithPagination;
