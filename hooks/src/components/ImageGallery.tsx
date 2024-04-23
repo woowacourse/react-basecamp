603d2d1e0f0d92438b891fe6cc37ccc0b11a06ed
Seongjin Hong <hongseongjin.to@gmail.com>
2024-04-23 12:21:16 +0900

    feat: 3단계 step3 커스텀 훅 적용

--- hooks/src/components/ImageGallery.tsx
+++ hooks/src/components/ImageGallery.tsx
@@ -1,32 +1,9 @@
-import React, { useState, useEffect } from "react";
-
-const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY;
-
-interface Image {
-  id: number;
-  webformatURL: string;
-  tags: string;
-}
+import React, { useState } from "react";
+import useImageSearch from "../hooks/useImageSearch";
 
 const ImageGallery: React.FC = () => {
-  const [images, setImages] = useState<Image[]>([]);
   const [keyword, setKeyword] = useState("");
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const response = await fetch(
-          `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(keyword)}&image_type=photo`
-        );
-        const data = await response.json();
-        setImages(data.hits);
-      } catch (error) {
-        console.error("데이터 수신 에러: ", error);
-      }
-    };
-
-    if (keyword) fetchImages();
-  }, [keyword]);
+  const { images } = useImageSearch(keyword);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
