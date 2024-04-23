13bf8e3e7537208beef7b68dc39e4a83f1956e99
이태훈 <rhymint@gmail.com>
2024-04-23 13:02:48 +0900

    chore: hooks mission 완료

--- hooks/src/components/ImageGallery.tsx
+++ hooks/src/components/ImageGallery.tsx
@@ -1,26 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY;
-
-interface Image {
-  id: number;
-  webformatURL: string;
-  tags: string;
-}
+import useImageSearchWithPagination from "../hooks/useImageSearchWithPagination";
 
 const ImageGallery: React.FC = () => {
-  const [images, setImages] = useState<Image[]>([]);
   const [keyword, setKeyword] = useState("");
-
-  // useEffect를 사용하여 keyword가 변경될 때마다 이미지를 가져오는 로직을 작성해보세요.
-  useEffect(() => {
-    // 여기에 코드를 작성하세요.
-    // 힌트:
-    // 1. fetchImages 함수를 정의하세요.
-    // 2. fetchImages 함수 내부에서 Pixabay API를 호출하여 이미지를 가져오세요.
-    // 3. 가져온 이미지를 상태(images)에 업데이트하세요.
-    // 4. keyword가 존재할 때만 fetchImages 함수를 호출하세요.
-  }, [keyword]);
+  const { images, currentPage, goToNextPage, goToPrevPage, isLoading, error } =
+    useImageSearchWithPagination(keyword);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -45,12 +30,29 @@ const ImageGallery: React.FC = () => {
         </form>
       </section>
       <section>
-        {images.map((image) => (
-          <article key={image.id}>
-            <img src={image.webformatURL} alt={image.tags} />
-            <p>{image.tags}</p>
-          </article>
-        ))}
+        <div>
+          <span>Page: {currentPage}</span>
+          <button onClick={goToPrevPage} disabled={currentPage === 1}>
+            Previous Page
+          </button>
+          <button onClick={goToNextPage}>Next Page</button>
+        </div>
+        {/* ... */}
+      </section>
+      <section>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>Error: {error.message}</p>}
+        {!isLoading && !error && (
+          <>
+            {images.length === 0 && <p>No images found.</p>}
+            {images.map((image) => (
+              <article key={image.id}>
+                <img src={image.webformatURL} alt={image.tags} />
+                <p>{image.tags}</p>
+              </article>
+            ))}
+          </>
+        )}
       </section>
     </>
   );
