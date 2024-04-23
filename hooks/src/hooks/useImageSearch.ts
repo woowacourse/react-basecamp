446d1d993490fbcdbff9e8180e8f383bce266d03
skiende74 <skiende74@gmail.com>
2024-04-23 20:33:47 +0900

    feat: 4단계까지 완료

---
+++ hooks/src/hooks/useImageSearch.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import useFetch from "./useFetch";
+
+interface FetchedImage {
+  total: number;
+  totalHits: number;
+  hits: Image[];
+}
+
+interface Image {
+  id: number;
+  webformatURL: string;
+  tags: string;
+}
+const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY;
+
+const useImageSearch = (keyword: string) => {
+  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${keyword}`;
+  const [images, setImages] = useState<Image[]>([]);
+  const { data, isLoading, error } = useFetch<FetchedImage>(url);
+  useEffect(() => {
+    if (data !== null) {
+      setImages(data.hits);
+    }
+  }, [data]);
+  return { images, isLoading, error };
+};
+
+export default useImageSearch;
