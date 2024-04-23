704206511ae46385a31e63a3e17c4b9f9f81381c
brgndy <brgndy96@gmail.com>
2024-04-23 12:29:49 +0900

    feat: useEffect 첫번째 기능 구현

--- hooks/src/components/ImageGallery.tsx
+++ hooks/src/components/ImageGallery.tsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY;
+
+const DEFAULT_API_URL = "https://pixabay.com/api/?key=";
 
 interface Image {
   id: number;
@@ -14,12 +16,23 @@ const ImageGallery: React.FC = () => {
 
   // useEffect를 사용하여 keyword가 변경될 때마다 이미지를 가져오는 로직을 작성해보세요.
   useEffect(() => {
-    // 여기에 코드를 작성하세요.
-    // 힌트:
-    // 1. fetchImages 함수를 정의하세요.
-    // 2. fetchImages 함수 내부에서 Pixabay API를 호출하여 이미지를 가져오세요.
-    // 3. 가져온 이미지를 상태(images)에 업데이트하세요.
-    // 4. keyword가 존재할 때만 fetchImages 함수를 호출하세요.
+    const fetchImages = async () => {
+      try {
+        const response = await fetch(
+          `${DEFAULT_API_URL}${API_KEY}&q=${encodeURIComponent(
+            keyword
+          )}&image_type=photo&pretty=true`
+        );
+        const data = await response.json();
+        setImages(data.hits);
+      } catch (error) {
+        console.error("Failed to fetch images:", error);
+      }
+    };
+
+    if (keyword) {
+      fetchImages();
+    }
   }, [keyword]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
