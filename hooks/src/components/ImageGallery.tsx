26e4cb44d52ba577f39252ab087f15082bdbcbde
Soyeon Choe <77609591+soi-ha@users.noreply.github.com>
2024-04-23 14:49:51 +0900

    [Custom Hook 미니 실습] 소하(최소연) 실습 제출합니다. (#70)
    * chore: Storybook 실행 연습
    
    * chore: EOL 삭제
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * feat: 커스텀 훅 구현하기
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ hooks/src/components/ImageGallery.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import useImageSearchWithPagination from '../hooks/useImageSearchWithPagination';
+
+const ImageGallery: React.FC = () => {
+	const [keyword, setKeyword] = useState('');
+	const { images, currentPage, goToNextPage, goToPrevPage } = useImageSearchWithPagination(keyword);
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setKeyword(e.currentTarget.keyword.value);
+	};
+	return (
+		<>
+			<header>
+				<h1>Image Gallery</h1>
+			</header>
+			<section>
+				<div>
+					<span>Page: {currentPage}</span>
+					<button onClick={goToPrevPage} disabled={currentPage === 1}>
+						Previous Page
+					</button>
+					<button onClick={goToNextPage}>Next Page</button>
+				</div>
+				<form onSubmit={handleSubmit}>
+					<label htmlFor="searchKeyword">검색:</label>
+					<input type="text" id="searchKeyword" name="keyword" placeholder="키워드 입력" />
+					<button type="submit">Search</button>
+				</form>
+			</section>
+			<section>
+				{images.map((image) => (
+					<article key={image.id}>
+						<img src={image.webformatURL} alt={image.tags} />
+						<p>{image.tags}</p>
+					</article>
+				))}
+			</section>
+		</>
+	);
+};
+
+export default ImageGallery;
