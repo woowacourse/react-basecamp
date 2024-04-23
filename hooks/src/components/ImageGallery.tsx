91be2e7b24f15c824df96cdafb6e98ff94a566dd
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-04-23 16:18:14 +0900

    feat: useImageSearchWithPagination hook 정의 및 적용

--- hooks/src/components/ImageGallery.tsx
+++ hooks/src/components/ImageGallery.tsx
@@ -1,25 +1,28 @@
 import React, { useState } from "react";
-
-import useImageSearch from "../hooks/useImageSearch";
-import usePagination from "../hooks/usePagination";
+import useImageSearchWithPagination from "../hooks/useImageSearchWithPagination";
 
 const ImageGallery: React.FC = () => {
   const [keyword, setKeyword] = useState("");
-
-  const { currentPage, goToNextPage, goToPrevPage } = usePagination();
-  const { images, isLoading, error } = useImageSearch(keyword, currentPage);
+  const { images, currentPage, goToNextPage, goToPrevPage } =
+    useImageSearchWithPagination(keyword);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setKeyword(e.currentTarget.keyword.value);
   };
-
   return (
     <>
       <header>
         <h1>Image Gallery</h1>
       </header>
       <section>
+        <div>
+          <span>Page: {currentPage}</span>
+          <button onClick={goToPrevPage} disabled={currentPage === 1}>
+            Previous Page
+          </button>
+          <button onClick={goToNextPage}>Next Page</button>
+        </div>
         <form onSubmit={handleSubmit}>
           <label htmlFor="searchKeyword">검색:</label>
           <input
@@ -32,28 +35,12 @@ const ImageGallery: React.FC = () => {
         </form>
       </section>
       <section>
-        <div>
-          <span>Page: {currentPage}</span>
-          <button onClick={goToPrevPage} disabled={currentPage === 1}>
-            Previous Page
-          </button>
-          <button onClick={goToNextPage}>Next Page</button>
-        </div>
-      </section>
-      <section>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>Error: {error.message}</p>}
-        {!isLoading && !error && (
-          <>
-            {images.length === 0 && <p>No images found.</p>}
-            {images.map((image) => (
-              <article key={image.id}>
-                <img src={image.webformatURL} alt={image.tags} />
-                <p>{image.tags}</p>
-              </article>
-            ))}
-          </>
-        )}
+        {images.map((image) => (
+          <article key={image.id}>
+            <img src={image.webformatURL} alt={image.tags} />
+            <p>{image.tags}</p>
+          </article>
+        ))}
       </section>
     </>
   );
