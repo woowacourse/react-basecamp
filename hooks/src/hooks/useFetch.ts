c5a572a12bff0ee3b371652f6cba0758e660c25d
낙타/송재석 <106071687+Largopie@users.noreply.github.com>
2024-04-23 17:09:51 +0900

    [Custom Hook 미니 실습] 낙타(송재석) 미션 제출합니다. (#82)
    * docs: storybook 설치 경로 가이드 추가
    
    * storybook Input 및 Display 실습
    
    * feat: react hook 실습 미션 추가
    
    * feat: 이미지 fetching 기능 구현
    
    * feat: useImageSearch hooks 구현 및 분리
    
    * feat: useFetch hooks 구현
    
    * refactor: useImageSearch에 useFetch 사용
    
    * feat: usePagination hooks 구현
    
    * rename: useFetch 오타 수정
    
    * refactor: usePagination을 이용한 리팩토링
    
    * refactor: usePagination hook을 useImageSearch에 조합
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ hooks/src/hooks/useFetch.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = <T,>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        setData(data);
+      } catch (error) {
+        if (error instanceof Error) setError(error);
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
