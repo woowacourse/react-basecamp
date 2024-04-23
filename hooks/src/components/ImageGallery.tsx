5be411a5e4315f871747e78307411d0d710300ae
brgndy <brgndy96@gmail.com>
2024-04-23 12:32:58 +0900

    feat: useImage 로분리

--- hooks/src/components/ImageGallery.tsx
+++ hooks/src/components/ImageGallery.tsx
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
-
-const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY;
-
-const DEFAULT_API_URL = "https://pixabay.com/api/?key=";
+import useImage from "../hooks/useImage";
 
 interface Image {
   id: number;
@@ -11,29 +8,8 @@ interface Image {
 }
 
 const ImageGallery: React.FC = () => {
-  const [images, setImages] = useState<Image[]>([]);
   const [keyword, setKeyword] = useState("");
-
-  // useEffect를 사용하여 keyword가 변경될 때마다 이미지를 가져오는 로직을 작성해보세요.
-  useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const response = await fetch(
-          `${DEFAULT_API_URL}${API_KEY}&q=${encodeURIComponent(
-            keyword
-          )}&image_type=photo&pretty=true`
-        );
-        const data = await response.json();
-        setImages(data.hits);
-      } catch (error) {
-        console.error("Failed to fetch images:", error);
-      }
-    };
-
-    if (keyword) {
-      fetchImages();
-    }
-  }, [keyword]);
+  const { images } = useImage(keyword);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
