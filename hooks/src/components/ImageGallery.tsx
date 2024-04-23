a8792b2a284e6ef607f1910fe53f0d843f86938b
jayming66 <mjw8941@gmail.com>
2024-04-23 12:10:56 +0900

    feat: usePagination 훅을 사용

--- hooks/src/components/ImageGallery.tsx
+++ hooks/src/components/ImageGallery.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import useImageSearch from "../hooks/useImageSearch";
+import usePagination from "../hooks/usePagination";
 
 const ImageGallery: React.FC = () => {
   const [keyword, setKeyword] = useState("");
-  const { images } = useImageSearch(keyword);
+  const { currentPage, goToNextPage, goToPrevPage } = usePagination();
+  const { images, isLoading, error } = useImageSearch(keyword, currentPage);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -28,12 +30,29 @@ const ImageGallery: React.FC = () => {
         </form>
       </section>
       <section>
-        {images.map((image) => (
-          <article key={image.id}>
-            <img src={image.webformatURL} alt={image.tags} />
-            <p>{image.tags}</p>
-          </article>
-        ))}
+        <div>
+          <span>Page: {currentPage}</span>
+          <button onClick={goToPrevPage} disabled={currentPage === 1}>
+            Previous Page
+          </button>
+          <button onClick={goToNextPage}>Next Page</button>
+        </div>
+        {/* ... */}
+      </section>
+      <section>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>Error: {error.message}</p>}
+        {!isLoading && !error && (
+          <>
+            {images.length === 0 && <p>No images found.</p>}
+            {images.map((image) => (
+              <article key={image.id}>
+                <img src={image.webformatURL} alt={image.tags} />
+                <p>{image.tags}</p>
+              </article>
+            ))}
+          </>
+        )}
       </section>
     </>
   );
--- hooks/src/hooks/useImageSearch.ts
+++ hooks/src/hooks/useImageSearch.ts
@@ -13,12 +13,12 @@ interface ImageSearchResult {
   hits: Image[];
 }
 
-const useImageSearch = (keyword: string) => {
+const useImageSearch = (keyword: string, page: number) => {
   const [images, setImages] = useState<Image[]>([]);
   const { data, isLoading, error } = useFetch<ImageSearchResult>(
     `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(
       keyword
-    )}&image_type=photo&pretty=true`
+    )}&page=${page}&image_type=photo&pretty=true`
   );
 
   useEffect(() => {
