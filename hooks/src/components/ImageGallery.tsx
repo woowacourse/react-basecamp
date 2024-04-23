a241d563c498e64916866ef4fecebbda0152b7b2
Seongjin Hong <mail@seongjin.me>
2024-04-23 14:40:53 +0900

    [Custom Hook 미니 실습] 아르(홍성진) 실습 제출합니다. (#64)
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * feat: Display 컴포넌트 및 스토리 작성
    
    * feat: Display 컴포넌트 안에 Input 컴포넌트 중첩 삽입
    
    * feat: 2단계 step4 코드 작성 결과 반영
    
    * feat: 3단계 step2 커스텀 훅 생성
    
    * feat: 3단계 step3 커스텀 훅 적용
    
    * feat: 4단계 step2 useFetch 훅 생성
    
    * feat: 4단계 step4 useImageSearch 훅 코드 업데이트
    
    * feat: 5단계 step2 페이지네이션 훅 작성
    
    * feat: 5단계 페이지네이션 커스텀 훅 적용
    
    * feat: 6단계 useImageSearchWithPagination 훅 작성 및 적용
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

--- hooks/src/components/ImageGallery.tsx
+++ hooks/src/components/ImageGallery.tsx
@@ -1,26 +1,10 @@
-import React, { useState, useEffect } from "react";
-
-const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY;
-
-interface Image {
-  id: number;
-  webformatURL: string;
-  tags: string;
-}
+import React, { useState } from "react";
+import useImageSearchWithPagination from "../hooks/useImageSearchWithPagination";
 
 const ImageGallery: React.FC = () => {
-  const [images, setImages] = useState<Image[]>([]);
   const [keyword, setKeyword] = useState("");
-
-  // useEffect를 사용하여 keyword가 변경될 때마다 이미지를 가져오는 로직을 작성해보세요.
-  useEffect(() => {
-    // 여기에 코드를 작성하세요.
-    // 힌트:
-    // 1. fetchImages 함수를 정의하세요.
-    // 2. fetchImages 함수 내부에서 Pixabay API를 호출하여 이미지를 가져오세요.
-    // 3. 가져온 이미지를 상태(images)에 업데이트하세요.
-    // 4. keyword가 존재할 때만 fetchImages 함수를 호출하세요.
-  }, [keyword]);
+  const { images, currentPage, goToNextPage, goToPrevPage } =
+    useImageSearchWithPagination(keyword);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -43,6 +27,13 @@ const ImageGallery: React.FC = () => {
           />
           <button type="submit">Search</button>
         </form>
+      </section>
+      <section>
+        <p>현재 페이지 : {currentPage}</p>
+        <button onClick={goToPrevPage} disabled={currentPage <= 1}>
+          이전 페이지
+        </button>
+        <button onClick={goToNextPage}>다음 페이지</button>
       </section>
       <section>
         {images.map((image) => (
