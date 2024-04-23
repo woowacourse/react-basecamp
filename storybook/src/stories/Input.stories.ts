bf5cd2fb317935f03e03bcacc02217fcca0a4a66
Lee sang Yeop <mma7710@naver.com>
2024-04-23 12:48:53 +0900

    반영완료

--- storybook/src/stories/Input.stories.ts
+++ storybook/src/stories/Input.stories.ts
@@ -10,4 +10,17 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+	parameters: {
+		docs: {
+			description: {
+				story: '신용카드 컴포넌트',
+			},
+		},
+	},
+
+	args: {
+		cardNumberError: { isError: false, errorMessage: '' },
+		cardNumbers: ['', '', '', ''],
+	},
+};
