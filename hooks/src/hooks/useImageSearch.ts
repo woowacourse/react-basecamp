94898fa147a1cc1f64b64396a858fbdc66d36878
soosoo22 <lllllh00@naver.com>
2024-04-23 12:45:17 +0900

    feat: useImageSearch 훅 구현 - useFetch 훅을 사용하여 데이터를 가져옴 - data가 변경되면 useEffect 내부에서 images 상태를 업데이트한다.

---
+++ hooks/src/hooks/useImageSearch.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import useFetch from "./useFetch";
+
+const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY;
+
+interface Image {
+  id: number;
+  webformatURL: string;
+  tags: string;
+}
+
+interface ImageSearchResult {
+  hits: Image[];
+}
+
+const useImageSearch = (keyword: string, page: number) => {
+  const [images, setImages] = useState<Image[]>([]);
+  const { data, isLoading, error } = useFetch<ImageSearchResult>(
+    `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(
+      keyword
+    )}&page=${page}&image_type=photo&pretty=true`
+  );
+
+  useEffect(() => {
+    if (data) {
+      setImages(data.hits);
+    }
+  }, [data]);
+
+  return { images, isLoading, error };
+};
+
+export default useImageSearch;
