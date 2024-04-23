35888e231dbac389857c84db73eb8b10a85b5d76
anttiey <hanuuny@gmail.com>
2024-04-23 12:08:14 +0900

    feat: useEffect 이해하고 적용하기

--- hooks/src/components/ImageGallery.tsx
+++ hooks/src/components/ImageGallery.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY;
 
@@ -10,17 +10,25 @@ interface Image {
 
 const ImageGallery: React.FC = () => {
   const [images, setImages] = useState<Image[]>([]);
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState('');
 
   // useEffect를 사용하여 keyword가 변경될 때마다 이미지를 가져오는 로직을 작성해보세요.
   useEffect(() => {
-    // 여기에 코드를 작성하세요.
-    // 힌트:
-    // 1. fetchImages 함수를 정의하세요.
-    // 2. fetchImages 함수 내부에서 Pixabay API를 호출하여 이미지를 가져오세요.
-    // 3. 가져온 이미지를 상태(images)에 업데이트하세요.
-    // 4. keyword가 존재할 때만 fetchImages 함수를 호출하세요.
+    if (keyword) {
+      fetchImages().then((data) => {
+        setImages(data.hits);
+      });
+    }
   }, [keyword]);
+
+  const fetchImages = async () => {
+    const requestURL =
+      'https://pixabay.com/api/?key=' + API_KEY + '&q=' + encodeURIComponent(keyword);
+
+    const response = await fetch(requestURL);
+
+    return response.json();
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -35,12 +43,7 @@ const ImageGallery: React.FC = () => {
       <section>
         <form onSubmit={handleSubmit}>
           <label htmlFor="searchKeyword">검색:</label>
-          <input
-            type="text"
-            id="searchKeyword"
-            name="keyword"
-            placeholder="키워드 입력"
-          />
+          <input type="text" id="searchKeyword" name="keyword" placeholder="키워드 입력" />
           <button type="submit">Search</button>
         </form>
       </section>
