52df3709f3e7c21796593ad55303b04e9f591101
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-04-23 15:17:07 +0900

    [Custom Hook 미니 실습] 러기(박정우)실습 제출합니다. (#72)
    * feat: 스토리북 테스트
    
    * feat: 미션 제출
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * Remove env
    
    * 수업미션
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ hooks/src/components/ImageGallery.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import useImageHook from "../hooks/useImageHook";
+
+const ImageGallery: React.FC = () => {
+  const [keyword, setKeyword] = useState("");
+  const { images } = useImageHook(keyword);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setKeyword(e.currentTarget.keyword.value);
+  };
+
+  return (
+    <>
+      <header>
+        <h1>Image Gallery</h1>
+      </header>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="searchKeyword">검색:</label>
+          <input type="text" id="searchKeyword" name="keyword" placeholder="키워드 입력" />
+          <button type="submit">Search</button>
+        </form>
+      </section>
+      <section>
+        {images.map((image) => (
+          <article key={image.id}>
+            <img src={image.webformatURL} alt={image.tags} />
+            <p>{image.tags}</p>
+          </article>
+        ))}
+      </section>
+    </>
+  );
+};
+
+export default ImageGallery;
