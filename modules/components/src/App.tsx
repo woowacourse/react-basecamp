52bbbfe3c12e678faaa401b643a1a4c76c4ec4b9
Hyelim Choi <HyelimChoi01@gmail.com>
2024-04-30 12:00:25 +0900

    feat: button & useInput lib 배포

--- modules/components/src/App.tsx
+++ modules/components/src/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { Button } from 'compoents-hailey';
 
 function App() {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
   return (
     <>
       <h1>Component Modules</h1>
+      <Button label="Click me published" onClick={handleClick} />
     </>
   );
 }
