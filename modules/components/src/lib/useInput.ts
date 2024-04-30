1c2c018d0c330518083c0aac42cd0b53f1e3a74e
ss0526100 <ss0526100@gmail.com>
2024-04-30 11:53:22 +0900

    feat: 모듈 구현

---
+++ modules/components/src/lib/useInput.ts
@@ -0,0 +1,16 @@
+import { ChangeEvent, useState } from 'react';
+
+function useInput(initialValue: string = '') {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  return {
+    value,
+    onChange: handleChange,
+  };
+}
+
+export default useInput;
