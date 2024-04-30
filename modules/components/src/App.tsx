066055f362cf70a76eea9f068ed973e8cee68c47
badahertz52 <badahertz52@gmail.com>
2024-04-30 11:46:08 +0900

    feat: button component npm 배포

--- modules/components/src/App.tsx
+++ modules/components/src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Button } from "badahertz52_react_basecamp_compoents";
 import "./App.css";
 
 function App() {
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
   return (
-    <>
-      <h1>Component Modules</h1>
-    </>
+    <div>
+      <h1>Button Component Example</h1>
+      <Button label="Click me" onClick={handleClick} />
+    </div>
   );
 }
 
