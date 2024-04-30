a5a97f4fc5058d2471f6c8bdaf496c2f2cce1900
ImxYJL <111052302+ImxYJL@users.noreply.github.com>
2024-04-30 15:16:02 +0900

    [npm 배포] 올리(이예진) 미션 제출합니다. (#122)
    * docs: storybook 설치 경로 가이드 추가
    
    * init commit
    
    * Display 컴포넌트 추가
    
    * feat: react hook 실습 미션 추가
    
    * gitignore 업데이트
    
    * feat: 미니 미션 요구사항 구현
    
    * 4/30 실습
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ modules/components/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button } from "imxyjl-compoents";
+
+function App() {
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
+  return (
+    <div>
+      <h1>Button Component Example</h1>
+      <Button label="Click me" onClick={handleClick} />
+    </div>
+  );
+}
+
+export default App;
---
+++ modules/hooks/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useInput } from "imxyjl-hooks";
+
+function App() {
+  const { value, onChange } = useInput("");
+
+  return (
+    <div>
+      <h1>useInput Example</h1>
+      <input type="text" value={value} onChange={onChange} />
+      <p>Input value: {value}</p>
+    </div>
+  );
+}
+
+export default App;
