70142cbad2decc93dcbc66d6ec58d13ee3e827b2
KIM DAEUN <midekuna@gmail.com>
2024-04-30 11:55:10 +0900

    [npm 배포] 렛서(김다은) 미션 제출합니다. (#93)
    * chore: Storybook 기본 세팅
    
    * feat: placeholder를 인자로 받는 Input 컴포넌트 추가
    
    * test: Storybook에 Input 컴포넌트 렌더링 테스트 추가
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * feat: hooks 실습
    
    * docs: PR 템플릿 추가
    
    * chore: gitignore에 env 추가
    
    * feat: npm module 실습 추가
    
    * feat: useInput 훅 추가
    
    * feat: 재사용 가능한 button 컴포넌트 추가
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ modules/hooks/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import useInput from './lib/useInput';
+
+function App() {
+  const { value, onChange } = useInput('');
+
+  return (
+    <>
+      <h1>Hooks Modules</h1>
+      <input type="text" value={value} onChange={onChange} />
+      <p>Input value: {value}</p>
+    </>
+  );
+}
+
+export default App;
