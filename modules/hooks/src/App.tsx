ca3e48f589a25a70a310c4cbc2e2f6bfd5b3e474
Fe <64690761+chysis@users.noreply.github.com>
2024-04-30 12:18:37 +0900

    [npm 배포] 에프이(박철민) 미션 제출합니다. (#120)
    * feat(Display, Input): 스토리북 미니 실습 컴포넌트 추가
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * docs: PR 템플릿 추가
    
    * chore: gitignore에 env 추가
    
    * feat: 커스텀 훅을 사용해서 ImageGallery 페이지 구현
    
    * chore: env 파일 삭제
    
    * feat: npm module 실습 추가
    
    * feat: npm publish 실습
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ modules/hooks/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useInput } from 'chysis-use-input';
+import './App.css';
+
+function App() {
+  const { value, onChange } = useInput('');
+
+  return (
+    <div>
+      <h1>useInput Example</h1>
+      <input type='text' value={value} onChange={onChange} />
+      <p>Input value: {value}</p>
+    </div>
+  );
+}
+
+export default App;
