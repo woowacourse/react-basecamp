aece68ea4dc8a9d7e8d78eed95757946dcac1d28
ss0526100 <ss0526100@gmail.com>
2024-04-30 11:53:22 +0900

    feat: 모듈 구현

--- modules/hooks/src/App.tsx
+++ modules/hooks/src/App.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-import "./App.css";
+import './App.css';
+
+import React from 'react';
+import { useInput } from 'ninja-boy-rantaro';
 
 function App() {
+  const { value, onChange } = useInput('');
+
   return (
     <>
       <h1>Hooks Modules</h1>
+
+      <h1>Component Modules</h1>
+      <h1>useInput Example</h1>
+      <p>{value}</p>
+      <input type='text' value={value} onChange={onChange} />
     </>
   );
 }
