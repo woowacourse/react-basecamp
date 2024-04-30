f3570aefa11865135356eadac2f9a36912f2c54b
rbgksqkr <rbgks1937@naver.com>
2024-04-30 12:07:28 +0900

    feat: level2_2 npm 배포 연습

--- modules/hooks/src/App.tsx
+++ modules/hooks/src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import { useInput } from 'maru-input-hooks';
 
 function App() {
+  const { value, onChange } = useInput("");
+
   return (
     <>
-      <h1>Hooks Modules</h1>
+      <h1>useInput Example</h1>
+      <input type="text" value={value} onChange={onChange} />
+      <p>Input value: {value}</p>
     </>
   );
 }
