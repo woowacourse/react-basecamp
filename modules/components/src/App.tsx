c0b7accd6bcb45e4cb8a94f34da47a8e6daebf71
Pakxe <64801796+pakxe@users.noreply.github.com>
2024-04-30 11:53:51 +0900

    [npm 배포] 웨디(박세현) 미션 제출합니다. (#88)
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * docs: PR 템플릿 추가
    
    * chore: gitignore에 env 추가
    
    * feat: npm module 실습 추가
    
    * feat: 버튼 모듈 제작
    
    * feat: useInput 모듈 제작
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>
    Co-authored-by: YUNAH <61104776+anttiey@users.noreply.github.com>

--- modules/components/src/App.tsx
+++ modules/components/src/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { Button } from 'pakxe-button';
 
 function App() {
+  const handleClick = () => {
+    alert('빡버!');
+  };
+
   return (
     <>
       <h1>Component Modules</h1>
+      <Button label='빡세버튼' onClick={handleClick} />
     </>
   );
 }
