5204b3db34806d7485f9146424c5a03601a47a0c
jayming66 <mjw8941@gmail.com>
2024-04-30 11:48:42 +0900

    npm publish 실습

--- modules/components/src/App.tsx
+++ modules/components/src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+// import Button from "./lib/Button";
 import "./App.css";
+import { Button } from "mawi-button";
 
 function App() {
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
   return (
-    <>
-      <h1>Component Modules</h1>
-    </>
+    <div>
+      <h1>Button Component Example</h1>
+      {/* <Button label="Click me" onClick={handleClick} /> */}
+      <Button label="mawi-button" onClick={handleClick} />
+    </div>
   );
 }
 
