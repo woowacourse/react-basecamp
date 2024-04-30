4e88a95f1838dc32f46e2100e6366c7876508842
Lee sang Yeop <mma7710@naver.com>
2024-04-30 11:56:13 +0900

    feat: hook 배포 완료

--- modules/hooks/src/App.tsx
+++ modules/hooks/src/App.tsx
@@ -1,12 +1,16 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import { useInput } from 'darr-hooks';
 
 function App() {
-  return (
-    <>
-      <h1>Hooks Modules</h1>
-    </>
-  );
+	const { value, onChange } = useInput('');
+
+	return (
+		<div>
+			<h1>useInput Example</h1>
+			<input type="text" value={value} onChange={onChange} />
+			<p>Input value: {value}</p>
+		</div>
+	);
 }
 
 export default App;
