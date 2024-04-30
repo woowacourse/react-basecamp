26e1f79b0dafbe7003d2d048037303fb557986a3
Largopie <106071687+Largopie@users.noreply.github.com>
2024-04-30 11:26:31 +0900

    feat: Button 컴포넌트 구현

--- modules/components/src/App.tsx
+++ modules/components/src/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Button from './lib/Button';
 
 function App() {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
   return (
     <>
       <h1>Component Modules</h1>
+      <Button label='Click me' onClick={handleClick} />
     </>
   );
 }
