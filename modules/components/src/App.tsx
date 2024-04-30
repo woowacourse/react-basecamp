1ff05c4eca9db55d6ecbb12be0ce8d7c55ddbc18
hain-tain <hain7ce@gmail.com>
2024-04-30 11:36:11 +0900

    feat: react-basecamp-modules 모듈 배포 및 적용

--- modules/components/src/App.tsx
+++ modules/components/src/App.tsx
@@ -1,11 +1,18 @@
-import React from "react";
 import "./App.css";
 
+import { Button } from "react-basecamp-components";
+import React from "react";
+
 function App() {
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
   return (
-    <>
-      <h1>Component Modules</h1>
-    </>
+    <div>
+      <h1>Button Component Example</h1>
+      <Button label="Click me" onClick={handleClick} />
+    </div>
   );
 }
 
