7442456c2c9dfd0fad3a4ae2a462c4285f76bd11
jaeml06 <jaeml0630@gmail.com>
2024-04-30 12:06:24 +0900

    feat : npm 모듈 배포

--- modules/hooks/src/App.tsx
+++ modules/hooks/src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useInput } from "chico_jaeml_useinput";
 import "./App.css";
 
 function App() {
+  const { value, onChange } = useInput("");
+
   return (
-    <>
-      <h1>Hooks Modules</h1>
-    </>
+    <div>
+      <h1>useInput Example</h1>
+      <input type="text" value={value} onChange={onChange} />
+      <p>Input value: {value}</p>
+    </div>
   );
 }
 
