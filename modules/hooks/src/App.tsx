9ec12f99ecb617dbbb3cff634389a1f542bbd477
jinsil <jinsil378900@gmail.com>
2024-04-30 11:45:05 +0900

    feat: useInput 훅 배포

--- modules/hooks/src/App.tsx
+++ modules/hooks/src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import "./App.css";
+import { useInput } from "chlwlstlf-use-input";
 
 function App() {
+  const { value, onChange } = useInput("");
+
   return (
-    <>
-      <h1>Hooks Modules</h1>
-    </>
+    <div>
+      <h1>useInput Example</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+      <p>Input value: {value}</p>
+    </div>
   );
 }
 
