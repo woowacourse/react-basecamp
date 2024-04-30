77243a8dba418679d8bc8f2d1d9236bee0faf86a
cys4585 <dev.youngsu@gmail.com>
2024-04-30 12:02:29 +0900

    모듈 배포 실습 완료

--- modules/components/src/App.tsx
+++ modules/components/src/App.tsx
@@ -1,11 +1,18 @@
-import React from "react";
 import "./App.css";
 
+import { Button } from "suya-component-button-test";
+import React from "react";
+
 function App() {
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
   return (
-    <>
-      <h1>Component Modules</h1>
-    </>
+    <div>
+      <h1>Button Component Example</h1>
+      <Button label="Click me" onClick={handleClick} />
+    </div>
   );
 }
 
