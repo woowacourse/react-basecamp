d8645a2cd138fa0667d8c851945a3fd633f4f169
ooherin <555ohr@naver.com>
2024-04-30 12:00:28 +0900

    feat(npm): npm 배포 실습 완성

--- modules/components/src/App.tsx
+++ modules/components/src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
+import { Button } from "components-rian";
 
 function App() {
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
   return (
-    <>
-      <h1>Component Modules</h1>
-    </>
+    <div>
+      <h1>Button Component Example</h1>
+      <Button label="Click me" onClick={handleClick} />
+    </div>
   );
 }
 
--- modules/hooks/src/App.tsx
+++ modules/hooks/src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import useInput from "./lib/useInput";
 import "./App.css";
 
 function App() {
+  const { value, onChange } = useInput("");
+
   return (
-    <>
-      <h1>Hooks Modules</h1>
-    </>
+    <div>
+      <h1>useInput Example</h1>
+      <input type="text" value={value} onChange={onChange} />
+      <p>Input value: {value}</p>
+    </div>
   );
 }
 
