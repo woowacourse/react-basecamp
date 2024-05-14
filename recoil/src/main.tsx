0c0a46b8520f3be4e8d50c6b7af1781e2893dfb2
ss0526100 <ss0526100@gmail.com>
2024-05-14 12:28:13 +0900

    feat:recoil and api

--- recoil/src/main.tsx
+++ recoil/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './index.css';
+
+import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>
+);
