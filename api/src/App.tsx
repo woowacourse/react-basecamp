fe1239512fb5b3c9444d511aa0f566946b3d1196
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-14 11:39:13 +0900

    feat: 장바구니 데이터 가져오기

--- api/src/App.tsx
+++ api/src/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { Product } from './types';
+import { CartItem, Product } from './types';
 import { fetchProducts } from './api';
 import ProductList from './components/ProductList';
+import Cart from './components/Cart';
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
@@ -37,8 +39,9 @@ function App() {
 
   return (
     <div>
-      <h1>상품 목록</h1>
+      <h1>상품 목록 및 장바구니</h1>
       <ProductList products={products} />
+      <Cart items={cartItems} />
     </div>
   );
 }
--- api/src/api/index.ts
+++ api/src/api/index.ts
@@ -1,4 +1,4 @@
-import { Product } from '../types';
+import { CartItem, Product } from '../types';
 import { generateBasicToken } from '../utils/auth';
 
 
@@ -11,6 +11,21 @@ export async function fetchProducts(): Promise<Product[]> {
 
   if (!response.ok) {
     throw new Error('Failed to fetch products');
+  }
+
+  const data = await response.json();
+  return data.content;
+}
+
+export async function fetchCartItems(): Promise<CartItem[]> {
+  const token = generateBasicToken(USER_ID, USER_PASSWORD);
+  const response = await fetch(`${API_URL}/cart-items`, {
+    method: 'GET',
+    headers: { Authorization: token },
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch cart items');
   }
 
   const data = await response.json();
