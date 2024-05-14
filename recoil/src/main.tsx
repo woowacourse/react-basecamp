7db3104c64384d96f588df8bc0d8b6e6ce38e049
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-05-14 12:31:30 +0900

    [API & Recoil 실습] 헤인(방세린) 미션 제출합니다. (#133)
    * 스토리북 실습
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * docs: PR 템플릿 추가
    
    * remove: .env file from git
    
    * feat: 실습
    
    * chore: gitignore에 env 추가
    
    * feat: npm module 실습 추가
    
    * feat: react-basecamp-modules 모듈 배포 및 적용
    
    * feat: react-basecamp-hook 모듈 배포 및 적용
    
    * feat: Recoil 실습 미션 추가
    
    * feat: 장바구니 api 실습 추가
    
    * feat: 장바구니 실습
    
    * feat: recoil 실습
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ recoil/src/main.tsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>
+);
