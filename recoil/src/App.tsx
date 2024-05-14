30da977b46ad04b6fd206ae2faba9363c3c017ee
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-14 12:16:46 +0900

    feat(recoil): 장바구니 상품 개수 변경 기능 구현

--- recoil/src/App.tsx
+++ recoil/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
+import AddToCartButton from './components/AddToCartButton';
 import CartItemCount from './components/CartItemCount';
+import RemoveFromCartButton from './components/RemoveFromCartButton';
 
 function App() {
   return (
     <div>
       <h1>장바구니</h1>
       <CartItemCount />
+      <AddToCartButton />
+      <RemoveFromCartButton />
     </div>
   );
 }
---
+++ recoil/src/components/AddToCartButton.tsx
@@ -0,0 +1,14 @@
+import { useSetRecoilState } from 'recoil';
+import { cartItemCountState } from '../recoil/atom';
+
+function AddToCartButton() {
+  const setCount = useSetRecoilState(cartItemCountState);
+
+  const handleClick = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  return <button onClick={handleClick}>상품 추가</button>;
+}
+
+export default AddToCartButton;
