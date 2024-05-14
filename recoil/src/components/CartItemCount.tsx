8ddf4bda5b615651cfaaad1ac72e29ba63e51b89
chysis <chysiss@naver.com>
2024-05-14 12:18:19 +0900

    feat: 컴포넌트 구현

---
+++ recoil/src/components/CartItemCount.tsx
@@ -0,0 +1,9 @@
+import { useRecoilValue } from 'recoil';
+import { cartItemCountState } from '../recoil/atoms';
+
+const CartItemCount = () => {
+  const count = useRecoilValue(cartItemCountState);
+  return <div>장바구니 상품 개수: {count}</div>;
+};
+
+export default CartItemCount;
