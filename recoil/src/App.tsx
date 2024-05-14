bf35e670f9168a375a3146bcdff63c583a0c8766
YUNAH <61104776+anttiey@users.noreply.github.com>
2024-05-14 12:15:32 +0900

    [API & Recoil 실습] 파슬리(김윤아) 미션 제출합니다. (#125)
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * docs: PR 템플릿 추가
    
    * chore: gitignore에 env 추가
    
    * feat: npm module 실습 추가
    
    * feat: Recoil 실습 미션 추가
    
    * feat: 장바구니 api 실습 추가
    
    * feat: 장바구니 api 실습 진행
    
    * feat: Recoil 실습 진행
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ recoil/src/App.tsx
@@ -0,0 +1,28 @@
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './components/ErrorFallback';
+import CartItemCount from './components/CardItemCount';
+import AddToCartButton from './components/AddToCardButton';
+import RemoveFromCartButton from './components/RemoveFromCardButton';
+import CartTotalPrice from './components/CartTotalPrice';
+import ProductList from './components/ProductList';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>장바구니</h1>
+      <CartItemCount />
+      <AddToCartButton />
+      <RemoveFromCartButton />
+      <CartTotalPrice />
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <ProductList />
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+export default App;
