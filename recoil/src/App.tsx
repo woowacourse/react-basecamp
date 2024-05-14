da0330243c862ee165a151b87a46af3c7adde8db
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-14 12:06:57 +0900

    feat: 장바구니 상품 개수 변경 기능 구현

--- recoil/src/App.tsx
+++ recoil/src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import CartItemCount from "./components/CartItemCount";
+import AddToCartButton from "./components/AddToCartButton";
+import RemoveFromCartButton from "./components/RemoveFromCartButton";
 import "./App.css";
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div>
       <h1>장바구니</h1>
       <CartItemCount />
+      <AddToCartButton />
+      <RemoveFromCartButton />
     </div>
   );
 }
