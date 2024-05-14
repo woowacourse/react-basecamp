97fa8e5d4cd54077cc6466f4b235ddef7c50f3e9
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-14 12:36:14 +0900

    feat(recoil): Asynchronous Selector를 사용하여 장바구니 데이터 가져오기

--- recoil/src/App.tsx
+++ recoil/src/App.tsx
@@ -1,8 +1,12 @@
+import { Suspense } from 'react';
 import './App.css';
 import AddToCartButton from './components/AddToCartButton';
 import CartItemCount from './components/CartItemCount';
 import CartTotalPrice from './components/CartTotalPrice';
 import RemoveFromCartButton from './components/RemoveFromCartButton';
+import ProductList from './components/ProductList';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './components/ErrorFallback';
 
 function App() {
   return (
@@ -12,6 +16,12 @@ function App() {
       <AddToCartButton />
       <RemoveFromCartButton />
       <CartTotalPrice />
+
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <ProductList />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
