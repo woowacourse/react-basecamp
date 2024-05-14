0b42f4ce5af0e1f333d075f805372e2da290c350
novice0840 <111696934+novice0840@users.noreply.github.com>
2024-05-14 12:50:44 +0900

    [API & Recoil 실습] 포메(김우성) 미션 제출합니다. (#148)
    * chore: initial
    
    * feat: 인풋 디스플레이 컴포넌트 생성
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: setting
    
    * feat: react hook 실습 미션 추가
    
    * docs: PR 템플릿 추가
    
    * feat: 기능 구현 완료
    
    * chore: gitignore에 env 추가
    
    * feat: npm module 실습 추가
    
    * deploy
    
    * feat: hook publish
    
    * feat: Recoil 실습 미션 추가
    
    * feat: 장바구니 api 실습 추가
    
    * feat: API 실습 완료
    
    * feat: recoil 실습
    
    ---------
    
    Co-authored-by: brgndy <brgndy96@gmail.com>
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ recoil/src/App.tsx
@@ -0,0 +1,31 @@
+import React, { Suspense } from "react";
+import CartItemCount from "./components/CartItemCount";
+import AddToCartButton from "./components/AddToCartButton";
+import RemoveFromCartButton from "./components/RemoveFromCartButton";
+import CartTotalPrice from "./components/CartTotalPrice";
+import "./App.css";
+import ProductList from "./components/ProductList";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./components/ErrorFallback";
+
+function App() {
+  return (
+    <div>
+      <h1>장바구니</h1>
+      <CartItemCount />
+      <AddToCartButton />
+      <RemoveFromCartButton />
+      <CartTotalPrice />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProductList />
+      </Suspense>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <ProductList />
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+export default App;
