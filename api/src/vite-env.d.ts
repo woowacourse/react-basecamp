3af6e6cc5dd0e12b6e13f94383247b77c69b9d25
brgndy <brgndy96@gmail.com>
2024-05-14 12:31:11 +0900

    feat: api 구현

--- api/src/vite-env.d.ts
+++ api/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_USER_ID: string;
+  readonly VITE_USER_PASSWORD: string;
+  // 다른 환경 변수들도 여기에 추가할 수 있습니다.
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
