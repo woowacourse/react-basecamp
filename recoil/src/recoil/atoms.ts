150054a5aa83302d5707ec6ec1ae1cf1a8437972
cys4585 <dev.youngsu@gmail.com>
2024-05-14 11:55:47 +0900

    recoil step3 장바구니 상품 개수 표시 컴포넌트 작성

--- recoil/src/recoil/atoms.ts
+++ recoil/src/recoil/atoms.ts
@@ -1,6 +1,6 @@
 import { atom } from "recoil";
 
-export const cardItemCountState = atom<number>({
-  key: "cardItemCountState", // 식별자 (key는 애플리케이션 전체에서 고유해야 함)
+export const cartItemCountState = atom<number>({
+  key: "cartItemCountState", // 식별자 (key는 애플리케이션 전체에서 고유해야 함)
   default: 0, // 초기값
 });
