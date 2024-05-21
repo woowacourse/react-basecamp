155a0a673e05c9efb0721d755d77e5dcf102806c
anttiey <hanuuny@gmail.com>
2024-05-21 12:11:33 +0900

    feat: Coupon 실습 진행

---
+++ coupons/src/hooks/useCoupons/useCoupons.test.tsx
@@ -0,0 +1,20 @@
+import { renderHook } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
+import { useCoupons } from './useCoupons.ts';
+import { couponsState } from '../../recoil/atom';
+import { mockCoupons } from '../../mocks/coupons';
+
+describe('useCoupons', () => {
+  it('쿠폰 목록을 반환한다', () => {
+    const { result } = renderHook(() => useCoupons(), {
+      wrapper: ({ children }) => (
+        <RecoilRoot initializeState={({ set }) => set(couponsState, mockCoupons)}>
+          {children}
+        </RecoilRoot>
+      ),
+    });
+
+    expect(result.current.coupons).toBeDefined();
+    expect(result.current.coupons.length).toBe(4);
+  });
+});
