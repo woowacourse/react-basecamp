a2f98bda2a06cd2bce52bc1d01239ee140938852
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-21 12:20:31 +0900

    feat(coupons): 쿠폰 유효성 확인하기

---
+++ coupons/src/hooks/useCouponFinder.test.tsx
@@ -0,0 +1,23 @@
+import { renderHook } from '@testing-library/react';
+import { useCouponFinder } from './useCouponFinder';
+import { RecoilRoot } from 'recoil';
+import { couponState } from '../recoil/atom';
+import { mockCoupons } from '../mocks/coupons';
+
+describe('useCouponFinder', () => {
+  it('존재하는 쿠폰 코드로 쿠폰을 찾을 수 있다.', () => {
+    const { result } = renderHook(() => useCouponFinder(), {
+      wrapper: ({ children }) => <RecoilRoot initializeState={({ set }) => set(couponState, mockCoupons)}>{children}</RecoilRoot>,
+    });
+
+    expect(result.current.findCouponByCode('FIXED5000')).toBe(mockCoupons[0]);
+  });
+
+  it('존재하지 않는 쿠폰 코드로 쿠폰을 찾으면 undefined를 반환한다.', () => {
+    const { result } = renderHook(() => useCouponFinder(), {
+      wrapper: ({ children }) => <RecoilRoot initializeState={({ set }) => set(couponState, mockCoupons)}>{children}</RecoilRoot>,
+    });
+
+    expect(result.current.findCouponByCode('INVALID_CODE')).toBeUndefined();
+  });
+});
