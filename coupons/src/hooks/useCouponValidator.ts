a79c376c116dfa10ee309b71c0ebe9c138c07bf4
Seongjin Hong <mail@seongjin.me>
2024-05-21 12:28:10 +0900

    [쿠폰 적용 실습] 아르(홍성진) 미션 제출합니다. (#182)
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * docs: PR 템플릿 추가
    
    * chore: gitignore에 env 추가
    
    * feat: npm module 실습 추가
    
    * feat: Recoil 실습 미션 추가
    
    * feat: 장바구니 api 실습 추가
    
    * [Storybook 미니 실습] 아르(홍성진) PR 제출합니다. (#6)
    
    * feat: Storybook 미니 실습 환경 구성
    
    * feat: Display 컴포넌트 및 스토리 작성
    
    * feat: Input 컴포넌트 및 스토리 작성
    
    * feat: Display 컴포넌트 안에 Input 컴포넌트 중첩 삽입
    
    * [Custom Hook 미니 실습] 아르(홍성진) 실습 제출합니다. (#64)
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * feat: Display 컴포넌트 및 스토리 작성
    
    * feat: Display 컴포넌트 안에 Input 컴포넌트 중첩 삽입
    
    * feat: 2단계 step4 코드 작성 결과 반영
    
    * feat: 3단계 step2 커스텀 훅 생성
    
    * feat: 3단계 step3 커스텀 훅 적용
    
    * feat: 4단계 step2 useFetch 훅 생성
    
    * feat: 4단계 step4 useImageSearch 훅 코드 업데이트
    
    * feat: 5단계 step2 페이지네이션 훅 작성
    
    * feat: 5단계 페이지네이션 커스텀 훅 적용
    
    * feat: 6단계 useImageSearchWithPagination 훅 작성 및 적용
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>
    
    * feat: modules 미션 수행 결과 업데이트
    
    * feat: API 실습 내용 구현
    
    * feat: Recoil 실습 내용 구현
    
    * chore: coupons 프로젝트 초기 설정 추가
    
    * feat: Coupons 실습 진행
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ coupons/src/hooks/useCouponValidator.ts
@@ -0,0 +1,18 @@
+import { Coupon } from "../types/coupon";
+
+export const useCouponValidator = () => {
+  const isCouponExpired = (expirationDate: string) => {
+    const today = new Date();
+    const expiration = new Date(expirationDate);
+    return expiration < today;
+  };
+
+  const isCouponValid = (coupon: Coupon) => {
+    return !isCouponExpired(coupon.expirationDate);
+  };
+
+  return {
+    isCouponExpired,
+    isCouponValid,
+  };
+};
