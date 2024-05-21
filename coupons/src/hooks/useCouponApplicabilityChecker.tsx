aadfb5401091fcf2305c9790b10d8a98669fc408
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-21 15:51:16 +0900

    feat(coupons): 사용 가능 시간 조건 확인하기

--- coupons/src/hooks/useCouponApplicabilityChecker.tsx
+++ coupons/src/hooks/useCouponApplicabilityChecker.tsx
@@ -6,11 +6,22 @@ export const useCouponApplicabilityChecker = () => {
   const { findCouponByCode } = useCouponFinder();
   const { isCouponValid } = couponValidator();
 
-  const isCouponApplicable = (coupon: Coupon, totalAmount: number) => {
+  const isCouponApplicable = (coupon: Coupon, totalAmount: number, now: Date = new Date()) => {
     const targetCoupon = findCouponByCode(coupon.code);
     if (!targetCoupon || !isCouponValid(coupon)) return false;
 
     if (coupon.minimumAmount && totalAmount < coupon.minimumAmount) return false;
+
+    if (targetCoupon.availableTime) {
+      const [startHour, startMinute, startSecond] = targetCoupon.availableTime.start.split(':').map(Number);
+
+      const [endHour, endMinute, endSecond] = targetCoupon.availableTime.end.split(':').map(Number);
+
+      const startTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), startHour, startMinute, startSecond);
+      const endTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), endHour, endMinute, endSecond);
+
+      if (now < startTime || now > endTime) return false;
+    }
 
     return true;
   };
