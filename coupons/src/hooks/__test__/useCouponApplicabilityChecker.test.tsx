f3d1d13478a6038f2fb2226ef809d4b0937dd8cc
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-21 12:15:34 +0900

    feat: 사용 가능 시간 조건 확인

--- coupons/src/hooks/__test__/useCouponApplicabilityChecker.test.tsx
+++ coupons/src/hooks/__test__/useCouponApplicabilityChecker.test.tsx
@@ -32,4 +32,39 @@ describe("useCouponApplicabilityChecker", () => {
     });
     expect(result.current.isCouponApplicable(mockCoupons[2], 60000)).toBe(true);
   });
+
+  describe("쿠폰 사용 가능 시간 확인", () => {
+    it("사용 가능 시간 외에는 쿠폰 적용 불가", () => {
+      const testTime = new Date("2023-05-01T08:00:00");
+
+      const { result } = renderHook(() => useCouponApplicabilityChecker(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => set(couponsState, mockCoupons)}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+      expect(
+        result.current.isCouponApplicable(mockCoupons[3], 100000, testTime)
+      ).toBe(false);
+    });
+
+    it("사용 가능 시간 내에는 쿠폰 적용 가능", () => {
+      const testTime = new Date("2023-05-01T07:00:00");
+      const { result } = renderHook(() => useCouponApplicabilityChecker(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => set(couponsState, mockCoupons)}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+      expect(
+        result.current.isCouponApplicable(mockCoupons[3], 100000, testTime)
+      ).toBe(true);
+    });
+  });
 });
