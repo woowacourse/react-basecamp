16e486d241b2b1e636224dbc337feac4afd4907b
novice0840 <111696934+novice0840@users.noreply.github.com>
2024-05-21 12:42:02 +0900

    [쿠폰 적용 실습] 포메(김우성) 미션 제출합니다. (#188)
    * chore: initial
    
    * feat: 인풋 디스플레이 컴포넌트 생성
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * docs: PR 템플릿 추가
    
    * feat: 기능 구현 완료
    
    * chore: gitignore에 env 추가
    
    * feat: npm module 실습 추가
    
    * deploy
    
    * feat: hook publish
    
    * feat: Recoil 실습 미션 추가
    
    * feat: 장바구니 api 실습 추가
    
    * feat: API 실습 완료
    
    * feat: 장바구니 2단계 쿠폰 로직 TDD 실습 추가
    
    * feat: step 7
    
    ---------
    
    Co-authored-by: brgndy <brgndy96@gmail.com>
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ coupons/src/hooks/useCouponFinder.ts
@@ -0,0 +1,14 @@
+import { useRecoilValue } from "recoil";
+import { couponListSelector } from "../recoil/selectors";
+
+export const useCouponFinder = () => {
+  const coupons = useRecoilValue(couponListSelector);
+
+  const findCouponByCode = (code: string) => {
+    return coupons.find((coupon) => coupon.code === code);
+  };
+
+  return {
+    findCouponByCode,
+  };
+};
