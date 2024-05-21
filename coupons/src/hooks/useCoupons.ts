c8878a5bead7eee383e333656217fe488f55f987
ss0526100 <ss0526100@gmail.com>
2024-05-21 12:23:00 +0900

    feat:coupon 구현

---
+++ coupons/src/hooks/useCoupons.ts
@@ -0,0 +1,10 @@
+import { couponListSelector } from '../recoil/selectors';
+import { useRecoilValue } from 'recoil';
+
+export const useCoupons = () => {
+  const coupons = useRecoilValue(couponListSelector);
+
+  return {
+    coupons,
+  };
+};
