5875c5ca3192ec24acdf2a275754818ebde6a8c1
Soyeon Choe <soy2302ten@gmail.com>
2024-05-21 12:20:33 +0900

    feat: 쿠폰 적용 실습

---
+++ coupons/src/recoil/selectors.ts
@@ -0,0 +1,10 @@
+import { selector } from 'recoil';
+import { couponsState } from './atoms';
+
+export const couponListSelector = selector({
+	key: 'couponListSelector',
+	get: ({ get }) => {
+		const coupons = get(couponsState);
+		return coupons;
+	},
+});
