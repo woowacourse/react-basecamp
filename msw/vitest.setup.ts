fafc219c020f591cbfd620b054a46997292ddd45
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-27 21:36:01 +0900

    chore(msw): vitest 설정 파일에서 msw 서버 설정
    테스트 실행 전후에 msw를 관리하는 코드 작성

---
+++ msw/vitest.setup.ts
@@ -0,0 +1,13 @@
+import { server } from './src/mocks/server';
+
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: 'error' });
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
