d8e241e39a5872ad0f0ebf528c9cd195b14a5f59
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-27 22:10:01 +0900

    feat(msw): 페이지네이션 기능 구현

--- msw/src/hooks/useProducts.test.ts
+++ msw/src/hooks/useProducts.test.ts
@@ -3,6 +3,7 @@ import useProducts from './useProducts';
 import { server } from '../mocks/server';
 import { HttpResponse, http } from 'msw';
 import { PRODUCTS_ENDPOINT } from '../api/endpoint';
+import { act } from 'react';
 
 describe('useProducts', () => {
   describe('상품 목록 조회', () => {
@@ -19,21 +20,41 @@ describe('useProducts', () => {
 
       expect(result.current.loading).toBe(true);
     });
+
+    it('상품 목록 조회 중 에러 상태', async () => {
+      server.use(
+        http.get(PRODUCTS_ENDPOINT, () => {
+          return new HttpResponse(null, { status: 500 });
+        }),
+      );
+
+      const { result } = renderHook(() => useProducts());
+
+      await waitFor(() => {
+        expect(result.current.products).toEqual([]);
+        expect(result.current.loading).toEqual(false);
+        expect(result.current.error).toBeTruthy();
+      });
+    });
   });
 
-  it('상품 목록 조회 중 에러 상태', async () => {
-    server.use(
-      http.get(PRODUCTS_ENDPOINT, () => {
-        return new HttpResponse(null, { status: 500 });
-      }),
-    );
+  describe('페이지네이션', () => {
+    it('다음 페이지의 상품 4개를 추가로 불러온다.', async () => {
+      const { result } = renderHook(() => useProducts());
 
-    const { result } = renderHook(() => useProducts());
+      await waitFor(() => {
+        expect(result.current.products).toHaveLength(20);
+        expect(result.current.page).toBe(1);
+      });
 
-    await waitFor(() => {
-      expect(result.current.products).toEqual([]);
-      expect(result.current.loading).toEqual(false);
-      expect(result.current.error).toBeTruthy();
+      act(() => {
+        result.current.fetchNextPage();
+      });
+
+      await waitFor(() => {
+        expect(result.current.products).toHaveLength(24);
+        expect(result.current.page).toBe(2);
+      });
     });
   });
 });
--- msw/src/hooks/useProducts.ts
+++ msw/src/hooks/useProducts.ts
@@ -13,9 +13,12 @@ interface UseProductsResult {
   products: Product[];
   loading: boolean;
   error: unknown;
+  page: number;
+  fetchNextPage: () => void;
 }
 
 export default function useProducts(): UseProductsResult {
+  const [page, setPage] = useState(1);
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<unknown>(null);
@@ -23,10 +26,11 @@ export default function useProducts(): UseProductsResult {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch(PRODUCTS_ENDPOINT);
+        const limit = page === 1 ? 20 : 4;
+        const response = await fetch(`${PRODUCTS_ENDPOINT}?page=${page}&limit=${limit}`);
         const data = await response.json();
 
-        setProducts(data);
+        setProducts((prevProducts) => [...prevProducts, ...data]);
       } catch (error) {
         setError(error);
       } finally {
@@ -35,7 +39,11 @@ export default function useProducts(): UseProductsResult {
     };
 
     fetchProducts();
-  }, []);
+  }, [page]);
 
-  return { products, loading, error };
+  const fetchNextPage = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  return { products, loading, error, page, fetchNextPage };
 }
