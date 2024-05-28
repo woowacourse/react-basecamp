3b6a3c7a89ad7a75c1bc1eb575a7dd27844238b5
MYONG JAEWI <78201530+Jaymyong66@users.noreply.github.com>
2024-05-28 14:52:38 +0900

    [MSW 실습] 마스터위(명재위) 미션 제출합니다. (#234)
    * feat: Display storybook
    
    * feat: input 컴포넌트
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * feat: 기본 useImageSearch 훅 만들고 사용
    
    * feat: useFetch훅을 만들고 useImageSearch훅에서 사용
    
    * feat: usePagination 훅을 사용
    
    * feat: useFetch, usePagination훅을 조합하여 useImageSearchWithPagination 훅을 사용
    
    * docs: PR 템플릿 추가
    
    * delete: .env 파일 삭제
    
    * feat: gitignore 에 .env 추가
    
    * chore: gitignore에 env 추가
    
    * feat: npm module 실습 추가
    
    * npm publish 실습
    
    * feat: Recoil 실습 미션 추가
    
    * feat: 장바구니 api 실습 추가
    
    * feat: api 미션진행
    
    * feat: recoil 미션 진행
    
    * feat: 장바구니 2단계 쿠폰 로직 TDD 실습 추가
    
    * feat: msw 실습 미션 추가
    
    * msw 미션 제출
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

--- api/src/App.tsx
+++ api/src/App.tsx
@@ -15,9 +15,6 @@ function App() {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
-
-  console.log(import.meta.env.VITE_API_URL);
-
   const handleAddToCart = async (productId: number) => {
     try {
       await addCartItem(productId);
@@ -67,13 +64,13 @@ function App() {
     return <div>Error: {error.message}</div>;
   }
 
-return (
-  <div>
-    <h1>상품 목록 및 장바구니</h1>
-    <ProductList products={products} onAddToCart={handleAddToCart} />
-    <Cart items={cartItems} onRemoveItem={handleRemoveItem} />
-  </div>
-);
+  return (
+    <div>
+      <h1>상품 목록 및 장바구니</h1>
+      <ProductList products={products} onAddToCart={handleAddToCart} />
+      <Cart items={cartItems} onRemoveItem={handleRemoveItem} />
+    </div>
+  );
 }
 
 export default App;
