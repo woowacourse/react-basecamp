30e8863ade19e4acc239bee6f926a8c1d6017862
cys4585 <dev.youngsu@gmail.com>
2024-05-28 11:56:26 +0900

    msw: step8 페이지네이션 기능

--- msw/src/hooks/useProducts.test.ts
+++ msw/src/hooks/useProducts.test.ts
@@ -3,6 +3,7 @@ import useProducts from "./useProducts";
 import { server } from "../mocks/server";
 import { HttpResponse, http } from "msw";
 import { PRODUCTS_ENDPOINT } from "../api/endpoints";
+import { act } from "react";
 
 describe("useProducts", () => {
   describe("상품 목록 조회", () => {
@@ -33,6 +34,35 @@ describe("useProducts", () => {
         expect(result.current.products).toEqual([]);
         expect(result.current.loading).toBe(false);
         expect(result.current.error).toBeTruthy();
+      });
+    });
+  });
+
+  describe("페이지네이션", () => {
+    it("초기에 첫 페이지의 상품 20개를 불러온다", async () => {
+      const { result } = renderHook(() => useProducts());
+
+      await waitFor(() => {
+        expect(result.current.products).toHaveLength(20);
+        expect(result.current.page).toBe(1);
+      });
+    });
+
+    it("다음 페이지의 상품 4개를 추가로 불러온다", async () => {
+      const { result } = renderHook(() => useProducts());
+
+      await waitFor(() => {
+        expect(result.current.products).toHaveLength(20);
+        expect(result.current.page).toBe(1);
+      });
+
+      act(() => {
+        result.current.fetchNextPage();
+      });
+
+      await waitFor(() => {
+        expect(result.current.products).toHaveLength(24);
+        expect(result.current.page).toBe(2);
       });
     });
   });
