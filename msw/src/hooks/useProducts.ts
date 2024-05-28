dd7d81d4636761e4ff2f51d80a68f647acb5a50f
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-28 12:33:19 +0900

    feat: msw 실습 수업

---
+++ msw/src/hooks/useProducts.ts
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+
+import { fetchProducts } from '../api/products';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  category: string;
+}
+
+interface UseProductsResult {
+  products: Product[];
+  loading: boolean;
+  error: unknown;
+  page: number;
+  fetchNextPage: () => void;
+}
+
+export default function useProducts(): UseProductsResult {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<unknown>(null);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        setLoading(true);
+        const data = await fetchProducts(page, 4);
+        setTotalPages(data.totalPages);
+
+        setProducts((prevProducts) => [...prevProducts, ...data.content]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getProducts();
+  }, [page]);
+
+  const fetchNextPage = () => {
+    if (totalPages > page) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  return { products, loading, error, page, fetchNextPage };
+}
