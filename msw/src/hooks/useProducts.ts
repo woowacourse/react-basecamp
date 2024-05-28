bba52a506efb831212e81ff1813dca4e112a77b9
useon <yoosk4986@naver.com>
2024-05-28 12:40:40 +0900

    feat: 240528 msw 실습

---
+++ msw/src/hooks/useProducts.ts
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import { fetchProducts } from '../api/products';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  category: string;
+}
+
+interface UseProductsResult {
+  products: Product[];
+  loading: boolean;
+  error: unknown;
+  page: number;
+  fetchNextPage: () => void;
+}
+
+export default function useProducts(): UseProductsResult {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<unknown>(null);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        setLoading(true);
+        const data = await fetchProducts(page, 4);
+        setProducts((prevProducts) => [...prevProducts, ...data]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getProducts();
+  }, [page]);
+
+  const fetchNextPage = () => {
+    if (page === 21) return;
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  return { products, page, loading, error, fetchNextPage };
+}
