543a1b17716d8724533a9a87dd32e6521b4d89c9
cys4585 <dev.youngsu@gmail.com>
2024-05-28 11:44:35 +0900

    msw: step7 상품 목록 조회 중 에러 상태 테스트 작성하기

--- msw/src/hooks/useProducts.ts
+++ msw/src/hooks/useProducts.ts
@@ -12,22 +12,30 @@ interface Product {
 interface UseProductsResult {
   products: Product[];
   loading: boolean;
+  error: unknown;
 }
 
 export default function useProducts(): UseProductsResult {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<unknown>(null);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch(PRODUCTS_ENDPOINT);
-      const data = await response.json();
-      setProducts(data);
-      setLoading(false);
+      try {
+        const response = await fetch(PRODUCTS_ENDPOINT);
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        // 테스트 환경에서 status 500을 반환하는데 그걸 자동으로 에러로 처리해주는 것인가?
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchProducts();
   }, []);
 
-  return { products, loading };
+  return { products, loading, error };
 }
