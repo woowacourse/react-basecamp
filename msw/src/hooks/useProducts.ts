ae62b028d4792b9dd01d9fc8e60e621690e54e1a
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-28 12:38:26 +0900

    [MSW 실습] 빙봉(김윤경) 미션 제출합니다. (#223)
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * docs: PR 템플릿 추가
    
    * chore: gitignore에 env 추가
    
    * feat: npm module 실습 추가
    
    * feat: Recoil 실습 미션 추가
    
    * feat: 장바구니 api 실습 추가
    
    * feat: 장바구니 2단계 쿠폰 로직 TDD 실습 추가
    
    * feat: msw 실습 미션 추가
    
    * feat: msw 실습 구현
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ msw/src/hooks/useProducts.ts
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { fetchProducts } from "../api/products";
+
+const END_PAGE = 21;
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  category: string;
+}
+
+interface UseProductsResult {
+  products: Product[];
+  loading: boolean;
+  error: unknown;
+  page: number;
+  fetchNextPage: () => void;
+}
+
+export default function useProducts(): UseProductsResult {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<unknown>(null);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        setLoading(true);
+        const data = await fetchProducts(page, 4);
+        setProducts((prevProducts) => [...prevProducts, ...data]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getProducts();
+  }, [page]);
+
+  const fetchNextPage = () => {
+    if (page < END_PAGE) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  return { products, loading, error, page, fetchNextPage };
+}
