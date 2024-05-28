88bf5af0ccf8ecac59603e2b84daac10eff27d05
해시 <108859281+dle234@users.noreply.github.com>
2024-05-28 12:39:13 +0900

    [MSW 실습] 해시(김가연) 미션 제출합니다. (#219)
    * test: storybook 연습
    
    * feat: react hook 실습 미션 추가
    
    * docs: PR 템플릿 추가
    
    * chore: gitignore에 env 추가
    
    * feat: npm module 실습 추가
    
    * feat: 버튼 모듈 배포
    
    * feat: useInput 모듈 생성
    
    * feat: Recoil 실습 미션 추가
    
    * feat: 장바구니 api 실습 추가
    
    * feat: recoil 사용 실습
    
    * feat: api 미션 완료
    
    * feat: 장바구니 2단계 쿠폰 로직 TDD 실습 추가
    
    * feat: 쿠폰 미션
    
    * feat: msw 실습 미션 추가
    
    * feat(msw): msw 연습
    
    * feat: loading
    
    ---------
    
    Co-authored-by: imakerjun <imakerjun@gmail.com>
    Co-authored-by: jaeml06 <107801932+jaeml06@users.noreply.github.com>

---
+++ msw/src/hooks/useProducts.ts
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../api/products";
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  category: string;
+}
+
+interface UseProductsResult {
+  products: Product[];
+  loading: boolean;
+  error: unknown;
+  page: number;
+  fetchNextPage: () => void;
+}
+
+export default function useProducts(): UseProductsResult {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<unknown>(null);
+  const [page, setPage] = useState(1);
+  const [isLast, setIsLast] = useState(false);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      if (page >= 21) setIsLast(true);
+      if (isLast) return;
+      try {
+        setLoading(true);
+        const data = await fetchProducts(page, 4);
+
+        setProducts((prevProducts) => [...prevProducts, ...data]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getProducts();
+  }, [page, isLast]);
+
+  const fetchNextPage = () => {
+    if (isLast) return;
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  return { products, loading, error, page, fetchNextPage };
+}
---
+++ msw/vitest.setup.ts
@@ -0,0 +1,12 @@
+import { server } from "./src/mocks/server";
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: "error" });
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
