bf8d362de3d4dc65bd5e53e11ba841f136870a16
rbgksqkr <rbgks1937@naver.com>
2024-06-04 14:25:30 +0900

    feat: react-query 베이스캠프 구현

---
+++ react-query/src/hooks/useDeleteToCart.test.tsx
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { act, renderHook, waitFor } from "@testing-library/react";
+import { useDeleteToCart } from "./useDeleteToCart";
+
+const queryClient = new QueryClient();
+
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+describe("useDeleteToCart", () => {
+  it("장바구니에 상품을 삭제할 수 있다", async () => {
+    const { result } = renderHook(() => useDeleteToCart(), { wrapper });
+
+    act(() => {
+      result.current.mutate(3);
+    });
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+  });
+});
