7e246e0569dde3f80d139d2db9fa35e4daf1f7d3
차승하 <75566149+ss0526100@users.noreply.github.com>
2024-06-04 11:39:44 +0900

    [MSW 실습] 소파(차승하) 미션 제출합니다. (#222)
    * feat:input story 구현
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * docs: PR 템플릿 추가
    
    * feat: useImage 구현
    
    * Delete hooks/.env
    
    * chore: gitignore에 env 추가
    
    * feat: npm module 실습 추가
    
    * feat:충돌방지
    
    * feat: 모듈 구현
    
    * feat: Recoil 실습 미션 추가
    
    * feat: 장바구니 api 실습 추가
    
    * feat: 모듈 구현
    
    * feat:input story 구현
    
    * feat: useImage 구현
    
    * Delete hooks/.env
    
    * feat:충돌방지
    
    * [Custom Hook 미니 실습] 소파(차승하) 실습 제출합니다. (#53)
    
    * feat:input story 구현
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * feat: useImage 구현
    
    * Delete hooks/.env
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>
    
    * feat:recoil and api
    
    * feat: 장바구니 2단계 쿠폰 로직 TDD 실습 추가
    
    * feat: 모듈 구현
    
    * feat:input story 구현
    
    * feat: useImage 구현
    
    * Delete hooks/.env
    
    * feat:충돌방지
    
    * [Custom Hook 미니 실습] 소파(차승하) 실습 제출합니다. (#53)
    
    * feat:input story 구현
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * feat: useImage 구현
    
    * Delete hooks/.env
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>
    
    * [Custom Hook 미니 실습] 소파(차승하) 실습 제출합니다. (#53)
    
    * feat:input story 구현
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * feat: useImage 구현
    
    * Delete hooks/.env
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>
    
    * feat:recoil and api
    
    * feat:coupon 구현
    
    * feat: msw 실습 미션 추가
    
    * feat: 모듈 구현
    
    * feat:input story 구현
    
    * feat: useImage 구현
    
    * Delete hooks/.env
    
    * feat:충돌방지
    
    * [Custom Hook 미니 실습] 소파(차승하) 실습 제출합니다. (#53)
    
    * feat:input story 구현
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * feat: useImage 구현
    
    * Delete hooks/.env
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>
    
    * [Custom Hook 미니 실습] 소파(차승하) 실습 제출합니다. (#53)
    
    * feat:input story 구현
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * feat: useImage 구현
    
    * Delete hooks/.env
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>
    
    * feat:recoil and api
    
    * [Custom Hook 미니 실습] 소파(차승하) 실습 제출합니다. (#53)
    
    * feat:input story 구현
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * feat: useImage 구현
    
    * Delete hooks/.env
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>
    
    * [Custom Hook 미니 실습] 소파(차승하) 실습 제출합니다. (#53)
    
    * feat:input story 구현
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * feat: useImage 구현
    
    * Delete hooks/.env
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>
    
    * feat:coupon 구현
    
    * msw 구현
    
    * msw 쓸모없는 데이터 추가
    
    * Update .gitignore
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ msw/src/mocks/handlers.ts
@@ -0,0 +1,19 @@
+import { HttpResponse, http } from 'msw';
+
+import { PRODUCTS_ENDPOINT } from '../api/endpoints';
+import products from './products.json';
+
+export const handlers = [
+  http.get(PRODUCTS_ENDPOINT, ({ request }) => {
+    const url = new URL(request.url);
+
+    const page = Number(url.searchParams.get('page') || '1');
+    const limit = page === 1 ? 20 : 4;
+    const start = page === 1 ? 0 : (page - 2) * 4 + 20;
+    const end = start + limit;
+
+    const paginatedProducts = products.slice(start, end);
+
+    return HttpResponse.json(paginatedProducts);
+  }),
+];
