4e1c8a3a1d6018f394558c1e458424aa4e9f9e1d
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2024-06-04 12:06:19 +0900

    [React-Query 실습] 쿠키(김진호) 미션 제출합니다. (#245)
    * storybook: storybook 체험
    
    * docs: storybook 설치 경로 가이드 추가
    
    * feat: react hook 실습 미션 추가
    
    * docs: PR 템플릿 추가
    
    * feat(all): 모든 기능 구현 (오둥컨벤션)
    
    * chore: gitignore에 env 추가
    
    * fix: env 안 보이게 설정
    
    * remove: env 파일 깃에서 삭제
    
    * chore: 스토리북 기존 내용 복구
    
    * feat: npm module 실습 추가
    
    * feat: 라이브러리 셋팅
    
    * 라이브러리 배포완료
    
    * feat(all): useInput 배포 모든 기능 구현 (오둥컨벤션)
    
    * feat: Recoil 실습 미션 추가
    
    * feat: 장바구니 api 실습 추가
    
    * feat: 모든 기능 구현 완료(오둥컨벤션)
    
    * feat: 장바구니 2단계 쿠폰 로직 TDD 실습 추가
    
    * feat: 저번 미션 커밋
    
    * feat: 쿠폰 적용 tdd 구현
    
    * feat: msw 실습 미션 추가
    
    * feat: msw 실습 완료
    
    * feat: react query 실습 추가
    
    * feat: react-query 실습 완료 (오둥)
    
    ---------
    
    Co-authored-by: 임동준 <im1@woowahan.com>
    Co-authored-by: imakerjun <imakerjun@gmail.com>

---
+++ react-query/src/mocks/handlers.ts
@@ -0,0 +1,29 @@
+import { http, HttpResponse } from "msw";
+import products from "./products.json";
+import { API_ENDPOINTS } from "../constants/api";
+import { CartItem } from "../types";
+
+export const handlers = [
+  http.get(API_ENDPOINTS.PRODUCTS, () => {
+    return HttpResponse.json(products);
+  }),
+  
+  http.post(
+    `${API_ENDPOINTS.CART}`,
+    async ({ request }: { request: { json: () => Promise<CartItem> } }) => {
+      const newCartItem = await request.json();
+      return HttpResponse.json(newCartItem, { status: 201 });
+    }
+  ),
+
+  http.delete(
+    `${API_ENDPOINTS.CART}/:cartItemId`,
+    async ({ request }) => {
+      const url = new URL(request.url);
+      const urlSplitBySlash = url.pathname.split('/');
+      const cartItemId = urlSplitBySlash[urlSplitBySlash.length - 1];      
+
+      return HttpResponse.json(`${cartItemId}가 삭제되었습니다`);
+    }
+  ),
+];
