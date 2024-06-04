a7f28bcdabe338bde3af64ce0a05d315df26d7fe
Largopie <106071687+Largopie@users.noreply.github.com>
2024-06-04 12:29:07 +0900

    feat(react-query): react-query를 활용한 장바구니 추가 기능 구현

---
+++ react-query/src/hooks/useAddCart.ts
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { API_ENDPOINTS } from '../constants/api';
+import { CartItem } from '../types';
+
+async function addToCart(item: CartItem): Promise<CartItem> {
+  const response = await fetch(API_ENDPOINTS.CART, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(item),
+  });
+
+  return await response.json();
+}
+
+export function useAddToCart() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: addToCart,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['cart'] });
+    },
+  });
+}
--- react-query/src/hooks/useAddToCart.test.tsx
+++ react-query/src/hooks/useAddToCart.test.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { act, renderHook, waitFor } from '@testing-library/react';
 import { ReactNode } from 'react';
 import { CartItem } from '../types';
+import { useAddToCart } from './useAddCart';
 
 const queryClient = new QueryClient();
 const wrapper = ({ children }: { children: ReactNode }) => <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
