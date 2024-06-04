f66201059e38a2ffdaf94a8b214710c9255423f5
jinsil <jinsil378900@gmail.com>
2024-06-04 12:23:31 +0900

    react-query 실습

---
+++ react-query/src/hooks/useDeleteFromCart.ts
@@ -0,0 +1,14 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { QUERY_KEYS } from "../constants/queryKeys";
+import { deleteFromCart } from "../api/carts";
+
+export function useDeleteFromCart() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteFromCart,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.CART] });
+    },
+  });
+}
