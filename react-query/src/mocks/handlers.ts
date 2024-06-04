9248156a23ec2cd87a445be373e3cee9d101c1a1
00kang <kdb070970@gmail.com>
2024-06-04 12:16:44 +0900

    feat : react-basecamp React Query 실습

---
+++ react-query/src/mocks/handlers.ts
@@ -0,0 +1,20 @@
+import { http, HttpResponse } from "msw";
+import { API_ENDPOINTS } from "../constants/api";
+import products from "./products.json";
+import { CartItem } from "../types";
+
+export const handlers = [
+  http.get(API_ENDPOINTS.PRODUCTS, () => {
+    return HttpResponse.json(products);
+  }),
+  http.post(
+    `${API_ENDPOINTS.CART}`,
+    async ({ request }: { request: { json: () => Promise<CartItem> } }) => {
+      const newCartItem = await request.json();
+      return HttpResponse.json(newCartItem, { status: 201 });
+    }
+  ),
+  http.delete(`${API_ENDPOINTS.CART}/:id`, () => {
+    return HttpResponse.json(null, { status: 204 });
+  }),
+];
