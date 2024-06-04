d35eff3026c014711cd4bd2eb325ef861c9f7ea1
Largopie <106071687+Largopie@users.noreply.github.com>
2024-06-04 12:20:55 +0900

    feat(react-query): 장바구니 추가 API 모킹

--- react-query/src/mocks/handlers.ts
+++ react-query/src/mocks/handlers.ts
@@ -1,9 +1,16 @@
 import { HttpResponse, http } from 'msw';
 import products from './products.json';
 import { API_ENDPOINTS } from '../constants/api';
+import { CartItem } from '../types';
 
 export const handlers = [
   http.get(API_ENDPOINTS.PRODUCTS, () => {
     return HttpResponse.json(products);
+  }),
+
+  http.post(`${API_ENDPOINTS.CART}`, async ({ request }: { request: { json: () => Promise<CartItem> } }) => {
+    const newCartItem = await request.json();
+
+    return HttpResponse.json(newCartItem, { status: 201 });
   }),
 ];
