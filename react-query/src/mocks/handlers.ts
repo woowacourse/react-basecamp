dc28c33bcc00030efe35bcb9d703392d02a98a82
hain-tain <hain7ce@gmail.com>
2024-06-04 14:04:22 +0900

    feat: 실습

---
+++ react-query/src/mocks/handlers.ts
@@ -0,0 +1,22 @@
+import { HttpResponse, http } from "msw";
+
+import { API_ENDPOINTS } from "../constants/api";
+import { CartItem } from "../types";
+import products from "./products.json";
+
+export const handlers = [
+  http.get(API_ENDPOINTS.PRODUCTS, () => {
+    return HttpResponse.json(products);
+  }),
+  http.post(
+    `${API_ENDPOINTS.CART}`,
+    async ({ request }: { request: { json: () => Promise<CartItem> } }) => {
+      const newCartItem = await request.json();
+      return HttpResponse.json(newCartItem, { status: 201 });
+    }
+  ),
+  http.delete(`${API_ENDPOINTS.CART}/:id`, async ({ params }) => {
+    const id = { id: Number(params.id) };
+    return HttpResponse.json(id, { status: 201 });
+  }),
+];
